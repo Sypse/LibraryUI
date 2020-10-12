4551c70a8be8030b13cc4491a91d8c4e87bfe4e3
Sypse <71714559+Sypse@users.noreply.github.com>
2020-10-12 18:11:17 +0200

    Update nigga.lua

--- nigga.lua
+++ nigga.lua
@@ -72,6 +72,7 @@ local function createOptionHolder(holderTitle, parent, parentTable, subHolder)
 		SliceScale = 0.04,
 		ClipsDescendants = true,
 		Parent = parent
+		library.ImageButton = holderTitle
 	})
 	
 	local round
@@ -715,6 +716,7 @@ local function createList(option, parent, holder)
 		SliceScale = 0.02,
 		Visible = false,
 		Parent = library.base
+		library.ImageButton = holderTitle
 	})
 	
 	local content = library:Create("ScrollingFrame", {
@@ -987,6 +989,7 @@ local function createColorPickerWindow(option)
 		SliceCenter = Rect.new(100, 100, 100, 100),
 		SliceScale = 0.02,
 		Parent = library.base
+		library.ImageButton = holderTitle
 	})
 		
 	local hue, sat, val = Color3.toHSV(option.color)
